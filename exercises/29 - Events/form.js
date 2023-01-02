0bf6089928538ab6f4bc53cae137832c971cfb49
NgTAnhMJB2306 <nta230620@gmail.com>
2023-01-02 23:53:18 +0700

    end module 5

--- exercises/29 - Events/form.js
+++ exercises/29 - Events/form.js
@@ -30,3 +30,20 @@ function logEvent(event) {
     console.log(event.type);
 }
 signupForm.name.addEventListener("keyup", logEvent);
+
+const photo = document.querySelector(".photo");
+photo.addEventListener("click", function () {
+    console.log("you clicked the photo");
+});
+
+function handlePhotoClick() {
+    console.log("you clicked the photo");
+}
+photo.addEventListener("click", handlePhotoClick);
+photo.addEventListener("keyup", handlePhotoClick);
+
+function handlePhotoClick(event) {
+    if (event.type === "clicked" || event.key === "Enter") {
+        console.log("you clicked the photo");
+    }
+}
